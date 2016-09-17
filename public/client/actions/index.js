162d31e86bc67e63901bac0fe3839b79f61e6e3e
andrewjmeng@gmail.com <andrewjmeng@gmail.com>
2016-09-16 19:58:02 -0700

    (feat) Adding modal

--- public/client/actions/index.js
+++ public/client/actions/index.js
@@ -1,16 +1,16 @@
-import axios from 'axios';
-
-export const FETCH_QUESTION = 'FETCH_QUESTION';
-const ROOT_URL = '';
-
-export function fetchQuestion(){
-  // const request = axios.get{`${ROOT_URL}/`};
-
-  return {
-    type: FETCH_QUESTION,
-    payload:request
-  };
-}
+// import axios from 'axios';
+//
+// export const FETCH_QUESTION = 'FETCH_QUESTION';
+// const ROOT_URL = '';
+//
+// export function fetchQuestion(){
+//   // const request = axios.get{`${ROOT_URL}/`};
+//
+//   return {
+//     type: FETCH_QUESTION,
+//     payload:request
+//   };
+// }
 
 export function selectQuestion(question) {
   console.log(question)
